06448b1ae74e40b1ba7d0ffea456c0e2eb4a84a6
rahmiagustina19 <42725490+rahmiagustina19@users.noreply.github.com>
2018-10-16 15:41:48 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,19 +1,10 @@
 <?php
-
 $servername = "localhost";
-
 $username = "root";
-
 $password = "";
-
 $db_name = "webdas";
-
 $conn = mysqli_connect($servername,$username,$password,$db_name);
-
 if (!$conn) {
-
     die("Gagal: " . mysqli_connect_error());
-
 }
-
 ?>
