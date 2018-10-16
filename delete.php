db35e8709307150f58b3d228cab16621299b8889
rahmiagustina19 <42725490+rahmiagustina19@users.noreply.github.com>
2018-10-16 15:43:43 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,23 +1,11 @@
 <?php
-
 session_start();
-
 require_once("db.php");
-
-
-
 $nim = $_GET["nim"];
-
 $sql = "DELETE FROM pendataan WHERE nim=$nim";
-
 if (mysqli_query($conn, $sql)) {
-
     header("Location: lihatdata.php");
-
 }else {
-
     echo "Gagal";
-
 }
-
 ?>
