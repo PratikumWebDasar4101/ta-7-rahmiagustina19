bc839a51da0214b0e6de573d221e1336c20ae156
rahmiagustina19 <42725490+rahmiagustina19@users.noreply.github.com>
2018-10-16 15:54:00 +0700

    Add files via upload

--- webdas.sql
+++ webdas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.8.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 15 Okt 2018 pada 05.52
--- Versi server: 10.1.34-MariaDB
--- Versi PHP: 7.2.7
+-- Waktu pembuatan: 16 Okt 2018 pada 10.27
+-- Versi server: 10.1.31-MariaDB
+-- Versi PHP: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -43,6 +43,16 @@ CREATE TABLE `pendataan` (
 --
 
 INSERT INTO `pendataan` (`nim`, `nama`, `jk`, `prodi`, `fakultas`, `asal`, `moto_hidup`) VALUES
+(5, 'Lolaawqihuryekwuq', 'Perempuan', '> D3 Sistem Informasi', 'FEB', 'Jakarta', 'Berusaha tdk lola'),
+(43, 'Siapa', 'Perempuan', '> D3 Sistem Informasi', 'FEB', 'London', 'kajahag'),
+(235, 'Agaa', 'Laki-laki', '> D3 Sistem Informasi', 'FEB', 'Jakarta', 'Gkgk'),
+(543, 'huhuhu', 'perempuan', 'D3 Sistem Informasi', 'FEB', 'Tasikmalaya', 'ahahah'),
+(654, 'intan', 'perempuan', 'D3 Manajemen Pemasaran', 'FEB', 'Jakarta', 'Bahagia always'),
+(1765, 'Roro', 'perempuan', 'D3 Sistem Informasi', 'FIT', 'Bandung', 'Lalal'),
+(6354, 'wanda afifah', 'perempuan', 'D3 Sistem Informasi', 'FEB', 'Jakarta', 'Ceria'),
+(6548, 'Amissu', 'perempuan', 'D3 Sistem Informasi', 'FEB', 'Jakarta', 'Heheh'),
+(7650, 'Budi', 'perempuan', 'D3 Sistem Informasi', 'FIT', 'Batam', 'Sukses'),
+(65431, 'Gilang', 'laki-laki', 'D3 Sistem Informasi', 'FEB', 'Bandung', 'Okeee'),
 (1234445, 'rahmi', 'perempuan', 'D3 Sistem Informasi', 'FIT', 'Tsm', 'aaa');
 
 --
